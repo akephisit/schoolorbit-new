f075cc15e7b2862841c67b6fc19ae40505a42d2f
kruakemaths <kruakemaths@gmail.com>
2025-12-07 11:17:56 +0700

    feat: Add Vite basic SSL plugin for local HTTPS development.

--- frontend-admin/vite.config.ts
+++ frontend-admin/vite.config.ts
@@ -1,7 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit(), basicSsl()],
+	server: {
+		host: true, // อนุญาตให้เข้าผ่าน domain name ได้
+		port: 5173,  // หรือ port ที่คุณใช้อยู่
+		hmr: {
+			host: 'local.schoolorbit.app',
+			port: 5173
+		}
+	}
 });
