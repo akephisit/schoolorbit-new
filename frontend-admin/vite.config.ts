7dbcc3ee3f04b80737162d290a349df279bec049
kruakemaths <kruakemaths@gmail.com>
2025-12-12 16:37:47 +0700

    build: Update Vite configuration for development and production environments.

--- frontend-admin/vite.config.ts
+++ frontend-admin/vite.config.ts
@@ -1,14 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		basicSsl()
-	],
+	plugins: [tailwindcss(), sveltekit(), basicSsl()],
 	server: {
-		https: true,
 		host: true, // อนุญาตให้เข้าผ่าน domain name ได้
 		port: 5173,  // หรือ port ที่คุณใช้อยู่
 		hmr: {
